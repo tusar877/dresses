1b0a9557dcc97f4790076bd0034aeeefe98ee632
tusar877 <130102846+tusar877@users.noreply.github.com>
2023-09-29 11:10:48 +0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,17 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   useEffect(() => {
-    window.location.href = 'https://amzn.to/3qrvViQ';
+    window.location.href = 'https://warriorplus.com/o2/a/kc2ykv/0';
   }, []);
 
-  return null;
+  return (
+    <>
+      <Head>
+        <title>Earn Money</title>
+        <meta name="description" content="Here you go" />
+      </Head>
+    </>
+  );
 };
 
 export default Home;
