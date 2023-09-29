b695c72509c40e5d6a930b7787966eb4d107cd94
tusar877 <130102846+tusar877@users.noreply.github.com>
2023-09-29 11:39:59 +0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,24 @@
 import { useEffect } from 'react';
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  useEffect(() => {
-    window.location.href = 'https://warriorplus.com/o2/a/kc2ykv/0';
-  }, []);
-
-  return null;
+  return (
+    <>
+      <Head>
+        <title>Your Page Title</title>
+      </Head>
+      <div className={styles.container}>
+        <iframe
+          src="https://prothomalo.com"
+          width="100%"
+          height="500"
+          title="Prothom Alo"
+        ></iframe>
+      </div>
+    </>
+  );
 };
 
 export default Home;
