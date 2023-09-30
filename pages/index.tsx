595318a8e132eb5dbcaae8026f765ea474b4620c
tusar877 <130102846+tusar877@users.noreply.github.com>
2023-09-30 10:56:02 +0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,7 +42,7 @@ const Home: NextPage = () => {
       <div className={styles.container}>
         <iframe
           id="embeddedIframe"
-          src="https://prothomalo.com"
+          src="https://mlwbd.com"
           title="Prothom Alo"
           allowFullScreen
         ></iframe>
